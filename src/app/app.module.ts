3317348a46282c97e3f2bad96673fb2d64a17986
Shane Rogers <swrogers@gmail.com>
2020-01-11 14:19:55 -0600

    New Material Design Module
    This module will be used to house any new Material Design components/modules
    that will be required.
    
    I've also added my notes org file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { MatButtonModule } from '@angular/material';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
